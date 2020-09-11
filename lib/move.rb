0334ef20b3164cd3623354e8ad07359ce6a73900
Jonathan Zavala <jontzavala@gmail.com>
2020-09-11 20:19:18 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,4 +5,8 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+user_input = [1,2,3,4,5,6,7,8,9]
+converted_input = [0,1,2,3,4,5,6,7,8]
 def input_to_index(user_input)
+  puts expect (conveted_input).to be _a (Integer)
+end

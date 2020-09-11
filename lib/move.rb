8340ccf88752d67b98d4ec8aeaa8fcc1171ffaf3
Jonathan Zavala <jontzavala@gmail.com>
2020-09-11 19:10:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(user_input)
-  expect "#{user_input["1"-"9"]}".to be_a(["0"-"8"])
+  expect "#{user_input["1"]}".to be_a(["0"-"8"])
 end

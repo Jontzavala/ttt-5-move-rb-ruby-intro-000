2198d09c928db981b279e0e964d47bf90c455b48
Jonathan Zavala <jontzavala@gmail.com>
2020-09-11 18:33:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(user_input)
-  expect"#{(5)}".to be_a(Integer)
+  expect"#{('5')}".to be_a(Integer)
 end

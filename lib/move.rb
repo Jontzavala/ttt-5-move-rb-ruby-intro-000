69571ce071652ef61158b645efcad579d8c7cdd9
Jonathan Zavala <jontzavala@gmail.com>
2020-09-11 18:54:57 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(user_input)
-  puts "#{user_input[1-9.to_i]}"
+  puts "#{user_input[Integer]}"
 end

e345a627de47a71a88b32c00805271cca17a3160
Jonathan Zavala <jontzavala@gmail.com>
2020-09-11 19:50:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,28 +5,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-it 'converts a user_input to an integer' do
-    user_input = "1"
-
-    converted_input = input_to_index(user_input)
-    expect(input_to_index(user_input)).to be_a(Integer)
-    expect(converted_input).to be_a(Integer)
-  end
-
-
-  it 'subtracts 1 from the user_input' do
-    user_input = "6"
-    converted_input = input_to_index(user_input)
-
-
-    expect(input_to_index(user_input)).to be(5)
-  end
-
-
-  it 'returns -1 for strings without integers' do
-    user_input = "invalid"
-
-    converted_input = input_to_index(user_input)
-    expect(input_to_index(user_input)).to be(-1)
-    expect(converted_input).to be(-1)
-  end

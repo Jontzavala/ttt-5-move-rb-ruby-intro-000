7f6a22a409f649fa28a9cbfa97d76ac7df76ebc9
Jonathan Zavala <jontzavala@gmail.com>
2020-09-11 20:05:20 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(user_input)
-  puts "converted_input = #{user_input[1]}"
+  puts expect "converted_input = #{user_input[1]}"
 end

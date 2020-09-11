94e12e9249cb099b2e39032794c47848e498f5f5
Jonathan Zavala <jontzavala@gmail.com>
2020-09-11 19:15:05 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(user_input)
-  expect "#{user_input["5" - "1"]}".to_i(["4"])
+  expect "#{user_input[5 - 1]}".to_i([4])
 end
